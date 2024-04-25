5921f606e9b13eff94485257ae9ed41425aba2c6
akramov1ch <shaxbozakramovic@gmail.com>
2024-04-25 15:40:10 +0500

    origin

--- cmd/main.go
+++ cmd/main.go
@@ -13,11 +13,25 @@ func main() {
 		fmt.Println("Username ni o`qishda xatolik yuz berdi:")
 		log.Fatal(err)
 	}
-	file, err := os.OpenFile("username.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile("storage/username.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err!= nil {
 		log.Fatal(err)
 	}
+	defer file.Close()
 	_, err = file.WriteString(username)
+	if err!= nil {
+		log.Fatal(err)
+	}
+	email, err := gt.GetEmail()
+	if err!= nil {
+		fmt.Println("Email ni o`qishda xatolik yuz berdi:")
+		log.Fatal(err)
+	}
+	_, err = file.WriteString("\n")
+	if err!= nil {
+		log.Fatal(err)
+	}
+	_, err = file.WriteString(email)
 	if err!= nil {
 		log.Fatal(err)
 	}
--- git/git.go
+++ git/git.go
@@ -1,12 +1,15 @@
 package git
 
 import (
-	"fmt"
-	"log"
 	"os/exec"
 )
 
 func GetUsername() (string, error){
 	username, err := exec.Command("git", "config", "user.name").Output()
 	return string(username), err
+}
+
+func GetEmail() (string, error){
+	email, err := exec.Command("git", "config", "user.email").Output()
+	return string(email), err
 }
